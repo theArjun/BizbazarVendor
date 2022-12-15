82fad9113bbeacd0d41ff4adc066bcb44ec15e2e
Sitaram Poudel <sitaram.poudel@bizbazar.com>
2022-12-15 14:22:43 +0545

    Adds loading component for lazy loading and and 404 not found page when user wants to go for undecleared url

--- src/component/SuspenseWrapper/SuspenseWrapper.jsx
+++ src/component/SuspenseWrapper/SuspenseWrapper.jsx
@@ -1,7 +1,22 @@
 import React, { lazy, Suspense } from "react";
+import { Space, Spin } from "antd";
 
 const SuspenseWrapper = (props) => {
-  return <Suspense fallback={<div>..loading</div>}>{props.children}</Suspense>;
+  return <Suspense fallback={<Loading />}>{props.children}</Suspense>;
 };
 
 export default SuspenseWrapper;
+
+const style = {
+  height: "40vh",
+  width: "40vw",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const Loading = () => (
+  <div style={style}>
+    <Spin size="large" />
+  </div>
+);
