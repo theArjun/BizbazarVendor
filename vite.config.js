db263094d15d68d4d0f9f3666b0143181c1950a7
amritach222 <amritach222@gamil.com>
2023-04-13 01:27:47 +0545

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   proxy: {
-  //     "/api": "https://dev.bizbazar.com.np/",
-  //     // "/images": "https://dev.bizbazar.com.np",
-  //   },
-  // },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": "https://dev.bizbazar.com.np/",
+      // "/images": "https://dev.bizbazar.com.np",
+    },
+  },
 
   preview: {
     port: 8080,
