2f8d8d26c5bb4e563005a506cef020f9584254c9
amritach222 <amritach222@gamil.com>
2023-04-13 02:03:09 +0545

    Updates

---
+++ assets/CategoryApi.971b319f.js
@@ -0,0 +1,1 @@
+import{A as e}from"./apiConfig.819e96ae.js";import{u as r}from"./useQuery.esm.484a4dde.js";const t=()=>r({queryKey:["categories"],queryFn:()=>e.get("categories")});export{t as u};
---
+++ assets/collapse.4ef0eee8.js
@@ -0,0 +1,3 @@
+import{f as n}from"./index.559c1fcf.js";var t=function(o){var i;return n({},o.componentCls,(i={},n(i,o.antCls+"-motion-collapse-legacy",{overflow:"hidden","&-active":{transition:"height "+o.motionDurationMid+" "+o.motionEaseInOut+`,
+        opacity `+o.motionDurationMid+" "+o.motionEaseInOut+" !important"}}),n(i,o.antCls+"-motion-collapse",{overflow:"hidden",transition:"height "+o.motionDurationMid+" "+o.motionEaseInOut+`,
+        opacity `+o.motionDurationMid+" "+o.motionEaseInOut+" !important"}),i))};const e=t;export{e as g};
